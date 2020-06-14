d5539bba0e2e2f6c7821a4fb8e98f0d9b428caef
Marina Permiakova <marina.mk.glazov@gmail.com>
2020-06-14 23:47:07 +0400

    Initial commit

---
+++ app/src/main/java/com/example/android/androidmarinapermiakova/MainActivity.java
@@ -0,0 +1,14 @@
+package com.example.android.androidmarinapermiakova;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.first_activity);
+    }
+}
